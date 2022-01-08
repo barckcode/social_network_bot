e0a66d80c6f99cddec3ea6c858da7f039f93b22f
BarckCode <barckcode@gmail.com>
2022-01-08 17:39:19 +0100

    Basic Usage Discord Bot

---
+++ bot.py
@@ -0,0 +1,30 @@
+import os, random
+from discord.ext import commands
+
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='99', help='Comando de prueba desde el Bot')
+async def nine_nine(ctx):
+    response = "Test de un comando desde Discord!"
+    await ctx.send(response)
+
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+
+bot.run(TOKEN)
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import discord
+
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '99!':
+        response = "Esto es una prueba desde el Bot!"
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+client.run(TOKEN)
